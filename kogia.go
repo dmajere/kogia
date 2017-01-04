007423cc5e55e9f4a7e89fe0cdc32b3082a25666
Dmitriy Fedorov <d_fedorov@wargaming.net>
2017-01-04 11:51:31 +0300

    Lite version of kogia, just reaping children

--- kogia.go
+++ kogia.go
@@ -1,81 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/signal"
-	"path"
-	"regexp"
-	"strings"
 	"syscall"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
-
-func loadEnv(env_file string) ([]string, error) {
-	var env []string
-	if env_file == "" {
-		return []string{}, nil
-	}
-
-	if buff, err := ioutil.ReadFile(env_file); err == nil {
-		for _, line := range strings.Split(strings.TrimSpace(string(buff)), "\n") {
-			if ok, err := regexp.MatchString(`^\w+=.*$`, line); ok {
-				env = append(env, line)
-				continue
-			} else if !ok {
-				log.WithField("line", line).Warning("Line is not match")
-			} else if err != nil {
-				return []string{}, err
-			}
-		}
-	} else {
-		return env, err
-	}
-	return env, nil
-}
-
-func startAndWait(command string, env_file string) ([]byte, error) {
-	cmd := exec.Command(command)
-	cmd.Env = os.Environ()
-
-	if data, err := loadEnv(env_file); err != nil {
-		log.WithField("EnvFile", env_file).Warning(err.Error())
-	} else {
-		cmd.Env = append(cmd.Env, data...)
-	}
-
-	out, err := cmd.CombinedOutput()
-	return out, err
-}
-
-func runDir(dir, env_file string) error {
-	listdir, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-
-	for _, ent := range listdir {
-		if !ent.IsDir() {
-			cmd := path.Join(dir, ent.Name())
-			out, err := startAndWait(cmd, env_file)
-			fields := log.Fields{
-				"Cmd": cmd,
-				"Env": env_file,
-			}
-			if err != nil {
-				log.WithFields(fields).Warning(err.Error())
-			} else {
-				log.WithFields(fields).Infof("Run Command Result %s", string(out))
-			}
-
-		}
-	}
-
-	return nil
-}
 
 func reapChildren() {
 	var (
@@ -121,27 +54,6 @@ func kogia_init(c *cli.Context) {
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
 
-	var env_file string
-
-	if c.Bool("skip-env") {
-		env_file = ""
-	} else {
-		env_file = c.String("env")
-		if env, err := loadEnv(env_file); err == nil {
-			cmd.Env = append(cmd.Env, env...)
-		}
-	}
-
-	if !c.Bool("skip-preinit") {
-		log.Info("Running PreInit")
-		if err := runDir(c.String("preinit"), env_file); err != nil {
-			log.WithFields(log.Fields{
-				"Task": "RunPreInit",
-				"Dir":  c.String("preinit"),
-			}).Warning(err.Error())
-		}
-	}
-
 	notify := make(chan os.Signal, 1024)
 	signal.Notify(notify, syscall.SIGCHLD, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
 
@@ -171,17 +83,6 @@ func kogia_init(c *cli.Context) {
 		log.Error(err.Error())
 		os.Exit(1)
 	}
-
-	if !c.Bool("skip-postinit") {
-		log.Info("Running PostInit")
-		if err := runDir(c.String("postinit"), env_file); err != nil {
-			log.WithFields(log.Fields{
-				"Task": "RunPostInit",
-				"Dir":  c.String("postinit"),
-			}).Warning(err.Error())
-		}
-	}
-
 	err = cmd.Wait()
 	if err != nil {
 		log.Error(err.Error())
--- main.go
+++ main.go
@@ -15,44 +15,11 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "kogia"
 	app.Usage = "Small and simple init system for docker containers"
-	app.Version = "0.11"
+	app.Version = "lite"
 	app.Author = "Dmitry Fedorov"
 	app.Email = "gmajere@gmail.com"
 
 	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   "skip-preinit, S",
-			Usage:  "Do not execute preinit scripts",
-			EnvVar: "KOGIA_SKIP_PREINIT",
-		},
-		cli.StringFlag{
-			Name:   "preinit, s",
-			Value:  "/etc/preinit.d",
-			Usage:  "Path to preinit scripts",
-			EnvVar: "KOGIA_PREINIT_DIR",
-		},
-		cli.BoolFlag{
-			Name:   "skip-postinit, P",
-			Usage:  "Do not execute postinit scripts",
-			EnvVar: "KOGIA_SKIP_POSTINIT",
-		},
-		cli.StringFlag{
-			Name:   "postinit, p",
-			Value:  "/etc/postinit.d",
-			Usage:  "Path to postinit scripts",
-			EnvVar: "KOGIA_POSTINIT_DIR",
-		},
-		cli.BoolFlag{
-			Name:   "skip-env, E",
-			Usage:  "Do not load additional env from files",
-			EnvVar: "KOGIA_SKIP_ENV",
-		},
-		cli.StringFlag{
-			Name:   "env, e",
-			Value:  "/etc/env",
-			Usage:  "Path to additional env file",
-			EnvVar: "KOGIA_ENV",
-		},
 		cli.StringFlag{
 			Name:   "level, l",
 			Value:  "warning",
